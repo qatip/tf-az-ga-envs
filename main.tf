bc858de227e51bf4fd8228dc89172d1db0e79bbd
qatip <michael2697@hotmail.com>
2025-08-20 04:34:30 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,8 @@
 resource "azurerm_resource_group" "demo" {
   name     = var.rg_name
   location = var.location
-  tags = merge(
-    {
-      Environment = var.environment,
-      Comment = "test"
-    },
-    var.common_tags
-  )
+  tags = {
+    Environment = var.environment,
+    Comment = "test"
+  }
 }
